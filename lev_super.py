b760805c4aa64718ed4880369678789000004afe
begimai <begimai095@gmail.com>
2022-10-05 12:41:16 +0600

    hello

---
+++ lev_super.py
@@ -0,0 +1,17 @@
+name = "Begimay"
+last_name = "Arstanbekoba"
+age = 19
+job = "no"
+university = "krsu"
+kurs = 3
+programm_kurs = "no"
+salary = "3000$" 
+
+print("My name is :", name)
+print("My last name is:", last_name)
+print("I`m:", age)
+print("Am i have a job?:", job)
+print("I`m study in:", university)
+print("Did i study programm kurs?:", programm_kurs)
+print("My salary:", salary)
+
